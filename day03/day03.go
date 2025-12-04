b10601c95aa688e049f353d0cb89929df24579f2
Peter <peter.delafosse@gmail.com>
2025-12-04 15:42:56 +1100

    Day 3 - Puzzle 2

--- day03/day03.go
+++ day03/day03.go
@@ -97,6 +97,91 @@ func (d AocDay3) Puzzle1(useSample int) {
 
 }
 
+const BANK_SIZE = 12
+
 func (d AocDay3) Puzzle2(useSample int) {
+
+	datafile := DIR + "data.txt"
+	if useSample == 1 {
+		datafile = DIR + "sample.txt"
+	}
+
+	f, err := os.Open(datafile)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	var (
+		line            string
+		bite            Bite
+		joltage, sum    int64
+		n, x, left, seq int
+	)
+
+	for scanner.Scan() {
+
+		line = scanner.Text()
+
+		n = len(line)
+		joltage = 0
+		seq = 0
+
+		if useSample > 0 {
+			fmt.Println("")
+			fmt.Println("BANK:", line)
+		}
+
+		h := &BiteHeap{}
+		heap.Init(h)
+
+		// Add first n digits leaving 1 less than the BANK_SIZE remaining
+		for x = 0; x < n-(BANK_SIZE-1); x++ {
+
+			heap.Push(h, Bite{
+				i: x,
+				b: line[x],
+			})
+
+		}
+
+		for seq < BANK_SIZE {
+
+			bite = heap.Pop(h).(Bite)
+
+			left = bite.i
+			joltage = (joltage * 10) + int64(bite.b-'0')
+			seq++
+
+			if useSample > 0 {
+				fmt.Println(seq, " > ", joltage)
+			}
+
+			// Remove any high numbers that sit to the left of our chosen digit
+			// Doing a crude "peak" to get the index
+			for h.Len() > 0 && (*h)[0].i < left {
+				_ = heap.Pop(h).(Bite)
+			}
+
+			if x < n {
+				// Push one more digit
+				heap.Push(h, Bite{
+					i: x,
+					b: line[x],
+				})
+				x++
+			}
+
+		}
+
+		sum += joltage
+
+	}
+
+	fmt.Println("")
+	fmt.Println("Total Joltage:", sum)
 
 }
--- day03/heap.go
+++ day03/heap.go
@@ -8,9 +8,16 @@ type Bite struct {
 
 type BiteHeap []Bite
 
-func (h BiteHeap) Len() int           { return len(h) }
-func (h BiteHeap) Less(i, j int) bool { return h[i].b > h[j].b } // Max heap
-func (h BiteHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h BiteHeap) Len() int { return len(h) }
+
+// Puzzle 2 adjustment, minimise index when bytes as equal
+func (h BiteHeap) Less(i, j int) bool {
+	if h[i].b == h[j].b {
+		return h[i].i < h[j].i
+	}
+	return h[i].b > h[j].b
+}
+func (h BiteHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
 
 func (h *BiteHeap) Push(x any) {
 	// Push and Pop use pointer receivers because they modify the slice's length,
