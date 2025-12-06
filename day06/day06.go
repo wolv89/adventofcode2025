1ba237566d0998bbbbe1350e6f0fb6143a8696e4
Peter <peter.delafosse@gmail.com>
2025-12-06 19:22:10 +1100

    Day 6 - Puzzle 2

--- day06/day06.go
+++ day06/day06.go
@@ -122,6 +122,127 @@ func (d AocDay6) Puzzle1(useSample int) {
 
 }
 
+type Problem struct {
+	nums int
+	op   byte
+}
+
 func (d AocDay6) Puzzle2(useSample int) {
+
+	datafile := DIR + "data.txt"
+	if useSample == 1 {
+		datafile = DIR + "sample.txt"
+	}
+
+	f, err := os.Open(datafile)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	var (
+		line                string
+		val, calc, total    int64
+		x, y, z, size, left int
+		operator            byte
+	)
+
+	data := make([]string, 0)
+	problems := make([]Problem, 0)
+
+	for scanner.Scan() {
+
+		line = scanner.Text()
+
+		if line[0] == OP_ADD || line[0] == OP_MUL {
+
+			for x = range line {
+				if line[x] != ' ' {
+					if size > 0 {
+						problems = append(problems, Problem{
+							nums: size,
+							op:   operator,
+						})
+					}
+					// Start at 0, don't count the operator as a space, just those in between
+					size = 0
+					operator = line[x]
+				} else {
+					size++
+				}
+			}
+
+			if size > 0 {
+				// No extra column here so add 1 or we're short
+				problems = append(problems, Problem{
+					nums: size + 1,
+					op:   operator,
+				})
+			}
+
+		} else {
+			data = append(data, line)
+		}
+
+	}
+
+	width := len(data) // Width, but top to bottom - 3 rows of data means each number is up to 3 digits wide
+	left = 0
+
+	for _, p := range problems {
+
+		if useSample > 0 {
+			fmt.Println("")
+			fmt.Println("PROBLEM: ", string(p.op))
+		}
+
+		values := make([][]byte, p.nums)
+		for y = range values {
+			values[y] = make([]byte, width)
+		}
+
+		for x, z = left, 0; x < left+p.nums; x, z = x+1, z+1 {
+			for y = 0; y < width; y++ {
+				values[z][y] = data[y][x]
+			}
+		}
+
+		calc = 0
+
+		for _, v := range values {
+
+			if useSample > 0 {
+				fmt.Println(string(v))
+			}
+
+			val, err = strconv.ParseInt(strings.TrimSpace(string(v)), 10, 64)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+
+			if calc == 0 {
+				calc = val
+			} else {
+				switch p.op {
+				case OP_ADD:
+					calc += val
+				case OP_MUL:
+					calc *= val
+				}
+			}
+
+		}
+
+		total += calc
+
+		left += p.nums + 1
+
+	}
+
+	fmt.Println("")
+	fmt.Println("Total:", total)
 
 }
